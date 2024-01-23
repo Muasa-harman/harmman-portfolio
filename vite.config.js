119f8ef2d9df236c2bce37a4ecb5a3252be18420
Harman Muasa <123101284+Muasa-harman@users.noreply.github.com>
2024-01-23 21:57:55 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
    build: {
-    outDir: 'dist/index.html',
+    outDir: 'dist',
     minify: 'terser',
   },
 })
